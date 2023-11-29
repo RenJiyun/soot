48ac122f8c64277594a9a789dbfedc97774434ea
renjiyun <1277260264@qq.com>
2023-11-29 15:13:03 +0800

    代码格式化

--- src/main/java/soot/Unit.java
+++ src/main/java/soot/Unit.java
@@ -10,12 +10,12 @@ package soot;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
@@ -34,56 +34,68 @@ import soot.util.Switchable;
  * Unit for their code. In general, a unit denotes some sort of unit for execution.
  */
 public interface Unit extends Switchable, Host, Serializable, Context {
-  /** Returns a list of Boxes containing Values used in this Unit. */
-  public List<ValueBox> getUseBoxes();
+    /**
+     * Returns a list of Boxes containing Values used in this Unit.
+     */
+    public List<ValueBox> getUseBoxes();
 
-  /** Returns a list of Boxes containing Values defined in this Unit. */
-  public List<ValueBox> getDefBoxes();
+    /**
+     * Returns a list of Boxes containing Values defined in this Unit.
+     */
+    public List<ValueBox> getDefBoxes();
 
-  /**
-   * Returns a list of Boxes containing Units defined in this Unit; typically branch targets.
-   */
-  public List<UnitBox> getUnitBoxes();
+    /**
+     * Returns a list of Boxes containing Units defined in this Unit; typically branch targets.
+     */
+    public List<UnitBox> getUnitBoxes();
 
-  /** Returns a list of Boxes pointing to this Unit. */
-  public List<UnitBox> getBoxesPointingToThis();
+    /**
+     * Returns a list of Boxes pointing to this Unit.
+     */
+    public List<UnitBox> getBoxesPointingToThis();
 
-  /** Adds a box to the list returned by getBoxesPointingToThis. */
-  public void addBoxPointingToThis(UnitBox b);
+    /**
+     * Adds a box to the list returned by getBoxesPointingToThis.
+     */
+    public void addBoxPointingToThis(UnitBox b);
 
-  /** Removes a box from the list returned by getBoxesPointingToThis. */
-  public void removeBoxPointingToThis(UnitBox b);
+    /**
+     * Removes a box from the list returned by getBoxesPointingToThis.
+     */
+    public void removeBoxPointingToThis(UnitBox b);
 
-  /** Clears any pointers to and from this Unit's UnitBoxes. */
-  public void clearUnitBoxes();
+    /**
+     * Clears any pointers to and from this Unit's UnitBoxes.
+     */
+    public void clearUnitBoxes();
 
-  /**
-   * Returns a list of Boxes containing any Value either used or defined in this Unit.
-   */
-  public List<ValueBox> getUseAndDefBoxes();
+    /**
+     * Returns a list of Boxes containing any Value either used or defined in this Unit.
+     */
+    public List<ValueBox> getUseAndDefBoxes();
 
-  public Object clone();
+    public Object clone();
 
-  /**
-   * Returns true if execution after this statement may continue at the following statement. GotoStmt will return false but
-   * IfStmt will return true.
-   */
-  public boolean fallsThrough();
+    /**
+     * Returns true if execution after this statement may continue at the following statement. GotoStmt will return false but
+     * IfStmt will return true.
+     */
+    public boolean fallsThrough();
 
-  /**
-   * Returns true if execution after this statement does not necessarily continue at the following statement. GotoStmt and
-   * IfStmt will both return true.
-   */
-  public boolean branches();
+    /**
+     * Returns true if execution after this statement does not necessarily continue at the following statement. GotoStmt and
+     * IfStmt will both return true.
+     */
+    public boolean branches();
 
-  public void toString(UnitPrinter up);
+    public void toString(UnitPrinter up);
 
-  /**
-   * Redirects jumps to this Unit to newLocation. In general, you shouldn't have to use this directly.
-   *
-   * @see PatchingChain#getNonPatchingChain()
-   * @see soot.shimple.Shimple#redirectToPreds(Chain, Unit)
-   * @see soot.shimple.Shimple#redirectPointers(Unit, Unit)
-   **/
-  public void redirectJumpsToThisTo(Unit newLocation);
+    /**
+     * Redirects jumps to this Unit to newLocation. In general, you shouldn't have to use this directly.
+     *
+     * @see PatchingChain#getNonPatchingChain()
+     * @see soot.shimple.Shimple#redirectToPreds(Chain, Unit)
+     * @see soot.shimple.Shimple#redirectPointers(Unit, Unit)
+     **/
+    public void redirectJumpsToThisTo(Unit newLocation);
 }
